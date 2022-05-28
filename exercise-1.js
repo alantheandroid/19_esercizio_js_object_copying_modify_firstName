a97d927645e4f7eddf6c68d2c5cad42ed2f5b624
alantheandroid <alessio.ilardi@gmail.com>
2022-05-28 12:42:37 +0200

    feat: add requested changes

--- exercise-1.js
+++ exercise-1.js
@@ -1,12 +1,19 @@
 const person1 = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 25
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
 };
 
 const person2 = person1;
 
 // Modifica la proprietà "firstName" di person2 in "Simon"
+
+person2.firstName = "Simon";
+
+/* viene modificato anche il valore del primo oggetto perché
+non abbiamo davvero creato una copia dell'oggetto ma solo 
+un passaggio per riferimento, ovvero entrambi gli oggetti
+puntano allo stesso spazio di memoria */
 
 console.log(person1);
 console.log(person2);
